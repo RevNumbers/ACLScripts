fb9c7e601f8c7b576288095c5d4a79d929225b35
RevNumbers <jegoodell@gmail.com>
2020-03-17 11:33:47 -0600

    Added scripts to run all, and run all in all subdirectories

---
+++ runAll-byDay.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for i in *
+  do
+    cd $i
+	echo "Running prepfiles.sh in $PWD"
+	./prepfiles.sh
+	echo "Running outbound_filter in $PWD"
+	./outbound_filter-nosubnet.sh VAL-CISCO-5525-P1-outbound-sorted-uniq
+	echo "Running inbound_filter in $PWD"
+	./inbound_filter-nosubnet.sh VAL-CISCO-5525-P1-inbound-sorted-uniq
+	echo "Running find_objects_outbound.sh in $PWD"
+	./find_objects_outbound.sh outbound_traffic-sorted-uniq.txt
+	echo "Running find_objects_inbound.sh in $PWD"
+	./find_objects_inbound.sh inbound_traffic-sorted-uniq.txt
+done
